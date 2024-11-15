ef12fd98e931249c157199b60ab47a83abb4fab0
SCP-iota <scp-iota@proton.me>
2024-11-14 19:38:24 -0600

    Remove unused import to Stack

--- src/main/java/com/cybernetic/SystemOperationsLog.java
+++ src/main/java/com/cybernetic/SystemOperationsLog.java
@@ -2,7 +2,6 @@ package com.cybernetic;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 public class SystemOperationsLog {
     private List<SystemOperation> stack = new ArrayList<>();
